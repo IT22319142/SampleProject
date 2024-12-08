270190ffcbd2ef77169364146e0bb61824c675c4
IT22319142 <it22319142@my.sliit.lk>
2024-12-08 22:00:47 +0530

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='bg-red-100'>Hello world </h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",  // For Vite's root index.html
+    "./src/**/*.{js,ts,jsx,tsx}", // For React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
